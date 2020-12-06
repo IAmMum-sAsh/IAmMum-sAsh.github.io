472fac106af5d39df0c2a5158f3d39fdc0859fdb
IAmMum-sAsh <56122911+IAmMum-sAsh@users.noreply.github.com>
2020-12-06 20:35:09 +0300

    commit

--- js/action.js
+++ js/action.js
@@ -1,29 +1,22 @@
-//var arr = { 0: "Хлубничка/300", 1: "Радуга/270", 2: "Бананка/270", 3: "Бетон/150", 4: "Солнышко/170" };
 var total = 0;
 var newBasket = new Map();
-newBasket.set("Хлубничка — 300", 0).set("Радуга — 270", 0).set("Бананка — 270", 0).set("Бетон — 150", 0).set("Солнышко — 170", 0);
+newBasket.set("Клубничный водоворот — 300", 0).set("Радуга — 270", 0).set("Бананка — 270", 0).set("Бетон — 150", 0).set("Солнышко — 170", 0);
 
 function add(item) {
     let tmp = item.split('/');
     total += Number(tmp[1]);
 
+    let a = "";
     for (let [key, value] of newBasket) {
         if (`${tmp[0]} — ${tmp[1]}` === key) {
             newBasket.set(key, ++value);
         }
-    }
-
-    let a = "";
-    for (let [key, value] of newBasket) {
         if (value != 0) {
             a += `<p>${key}&#8381;   [x${value}]</p>`;
         }
     }
 
     let start = document.getElementById("start");
-    if (start.innerHTML === "Корзина пуста") {
-        start.className = "no";
-    }
     start.innerHTML = a;
 
     let right = document.getElementById("right");
@@ -32,50 +25,13 @@ function add(item) {
 
 
 
-
-// var arr = { 0: "Клубничный рожок/300", 1: "Рожок «Радуга»/270", 2: "Бананка/270", 3: "Бетон/150", 4: "Солнышко/170" };
-// var total = 0;
-// var newBasket = new Map();
-// newBasket.set("Клубничный рожок — 300", 0).set("Рожок «Радуга» — 270", 0).set("Бананка — 270", 0).set("Бетон — 150", 0).set("Солнышко — 170", 0);
-
-// function add(item) {
-//     let tmp = arr[item].split('/');
-//     total += Number(tmp[1]);
-
-//     for (let [key, value] of newBasket) {
-//         if (`${tmp[0]} — ${tmp[1]}` === key) {
-//             newBasket.set(key, ++value);
-//         }
-//     }
-
-//     let a = "";
-//     for (let [key, value] of newBasket) {
-//         if (value != 0) {
-//             a += `<p>${key}&#8381;   [x${value}]</p>`;
-//         }
-//     }
-
-//     let start = document.getElementById("start");
-//     if (start.innerHTML === "Корзина пуста") {
-//         start.className = "no";
-//     }
-//     start.innerHTML = a;
-
-//     let right = document.getElementById("right");
-//     right.innerHTML = `${total}`;
-// }
-
-
-
 function toWEB() {
     var viewport = document.querySelector("meta[name=viewport]");
 
     if (window.matchMedia("(min-width: 1024px)").matches) {
         viewport.setAttribute('content', 'width=device-width');
-        //alert("И так web");
     } else {
         viewport.setAttribute('content', 'width=1280px');
-        //alert("Теперь 1280px");
     }
 }
 
@@ -84,9 +40,7 @@ function toAPP() {
 
     if (window.matchMedia("(max-width: 639px)").matches) {
         viewport.setAttribute('content', 'width=device-width');
-        //alert("И так mobile");
     } else {
         viewport.setAttribute('content', 'width=425px');
-        //alert("Теперь 425px");
     }
 }
